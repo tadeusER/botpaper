4cc73cf7e26a836b6a269f4392260757a414192a
Enrique <eramirez@wmtechnologies.cl>
2023-09-02 16:31:24 -0400

    bot discord

--- src/chats/discord_bot.py
+++ src/chats/discord_bot.py
@@ -6,12 +6,41 @@ PREFIX = '!'
 
 bot = commands.Bot(command_prefix=PREFIX)
 
+# ... (importaciones y demás configuración)
+
+# Cargar configuraciones del archivo YAML
+with open('config.yaml', 'r') as file:
+    data = yaml.safe_load(file)
+
+schedules = data.get('schedules', [])
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name} ({bot.user.id})')
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send('Hello, world!')
+    for schedule in schedules:
+        # Asegurarse de que el canal exista
+        for guild in bot.guilds:
+            channel_name = schedule.get('channel')
+            exists = discord.utils.get(guild.channels, name=channel_name)
+            if not exists:
+                await guild.create_text_channel(channel_name)
+                print(f"Channel {channel_name} created.")
+
+        # Programar el cronjob
+        cron_time = schedule.get('cron_schedule', '')
+        if cron_time:
+            @aiocron.crontab(cron_time)
+            async def cronjob():
+                for keyword in schedule.get('search_keywords', []):
+                    # Aquí colocas la lógica para buscar los artículos con tu keyword
+                    # Supongamos que articles_search_function es la función que devuelve los artículos
+                    articles = await articles_search_function(keyword)
+                    
+                    channel_name = schedule.get('channel')
+                    channel = discord.utils.get(guild.channels, name=channel_name)
+                    for article in articles:
+                        await channel.send(article)  # Suponiendo que el artículo es un string
 
 bot.run(TOKEN)
+
