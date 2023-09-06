068262c40444253e1583cff82472e7a9acb6f8e6
Enrique <eramirez@wmtechnologies.cl>
2023-09-06 15:42:26 -0300

    correcion de mapper

--- src/mapper/mapper_response.py
+++ src/mapper/mapper_response.py
@@ -2,17 +2,22 @@
 
 from models.paper_model import ArticleMetadata
 from models.response_api import CambrigeResponse
+from typing import List
 
 
-def from_cambrige_response(response: CambrigeResponse) -> 'ArticleMetadata':
-    if not response.itemHits or len(response.itemHits) == 0:
+def from_cambrige_response(response: CambrigeResponse) -> List['ArticleMetadata']:
+    if not response.itemHits:
         raise ValueError("CambrigeResponse no tiene items")
 
-    item = response.itemHits[0].item
-    return ArticleMetadata(
-        id=item.id,
-        title=item.title,
-        abstract=item.abstract,
-        authors=item.authors,
-        # ... mapea otros campos según lo necesites
-    )
+    articles = []
+    for item_hit in response.itemHits:
+        item = item_hit.item
+        article = ArticleMetadata(
+            id=item.id,
+            title=item.title,
+            abstract=item.abstract,
+            authors=item.authors,
+        )
+        articles.append(article)
+
+    return articles
